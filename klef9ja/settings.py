d85e0a4d47e328430aa1a1a89466c9287e3c8f79
Adams Muhammed <muhammedadam4544@gmail.com>
2021-12-13 14:29:53 +0100

    database

--- klef9ja/settings.py
+++ klef9ja/settings.py
@@ -29,7 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+k__l*59u#0fwcf&h%)ioi9#z3vb935d&&+6@xsx%0^wbj5-sx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+DEBUG_PROPAGATE_EXCEPTIONS=True
 
 ALLOWED_HOSTS = ['final-klef.herokuapp.com', '127.0.0.1']
 
